dbe25fdeaf918922d3fc3a985599e1381c2cfe24
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-04-20 19:10:51 -0300

    feat(theme): integrate inter font family and typography tokens

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,34 @@
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_700Bold,
+  useFonts,
+} from "@expo-google-fonts/inter";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
 import { colors } from "@/theme/colors";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function Layout() {
+  const [areFontsLoaded, error] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold,
+  });
+
+  useEffect(() => {
+    if (areFontsLoaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [areFontsLoaded, error]);
+
+  if (!areFontsLoaded || !error) {
+    return null;
+  }
+
   return (
     <Stack
       screenOptions={{
