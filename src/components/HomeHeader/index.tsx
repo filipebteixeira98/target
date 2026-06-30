ed968f93dd9378d00e9d9090c2e18ce964ee14ea
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-06-30 14:52:47 -0300

    fix(home-header): align income and outcome summaries correctly

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -24,12 +24,12 @@ export function HomeHeader({ data }: Props) {
       colors={[colors.blue[500], colors.blue[800]]}
       style={styles.container}
     >
-      <View style={styles.summary}>
+      <View>
         <Text style={styles.label}>Total you own</Text>
         <Text style={styles.total}>{data.total}</Text>
       </View>
       <Separator color={colors.blue[400]} />
-      <View>
+      <View style={styles.summary}>
         <Summary
           data={data.input}
           icon={{ name: "arrow-upward", color: colors.green[500] }}
