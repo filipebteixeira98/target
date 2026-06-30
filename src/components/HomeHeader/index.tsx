d6d9ad9a35b4013b8bae1833d38eda4423799d73
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-06-30 13:26:20 -0300

    feat(home-header): add income and outcome summaries

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -2,6 +2,7 @@ import { LinearGradient } from "expo-linear-gradient";
 import { Text, View } from "react-native";
 
 import { Separator } from "@/components/Separator";
+import { Summary } from "@/components/Summary";
 
 import { colors } from "@/theme/colors";
 
@@ -21,11 +22,22 @@ export function HomeHeader({ data }: Props) {
       colors={[colors.blue[500], colors.blue[800]]}
       style={styles.container}
     >
-      <View>
+      <View style={styles.summary}>
         <Text style={styles.label}>Total you own</Text>
         <Text style={styles.total}>{data.total}</Text>
       </View>
       <Separator color={colors.blue[400]} />
+      <View>
+        <Summary
+          data={{ label: "Incoming", value: "$4.000,00" }}
+          icon={{ name: "arrow-upward", color: colors.green[500] }}
+        />
+        <Summary
+          isLeft
+          data={{ label: "Outcome", value: "-$1.400,00" }}
+          icon={{ name: "arrow-downward", color: colors.red[400] }}
+        />
+      </View>
     </LinearGradient>
   );
 }
